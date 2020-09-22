e6119fc84b8dc7638d133dd359788ca278f747fb
Gleb <neurothrop@gmail.com>
2020-09-23 00:13:46 +0300

    Better naming;
    Signed-off-by: Gleb <neurothrop@gmail.com>

--- AsyncQueue.py
+++ AsyncQueue.py
@@ -3,23 +3,23 @@ import asyncio
 
 class AsyncQueue:
     def __init__(self):
-        self.future = []
-        self.queue = []
+        self.futures_queue = []
+        self.messages_queue = []
 
     def add_message(self, message):
-        self.queue.append(message)
+        self.messages_queue.append(message)
         self.try_resolve()
 
     def get_message(self):
         future = asyncio.Future()
-        self.future.append(future)
+        self.futures_queue.append(future)
         self.try_resolve()
         return future
 
     def try_resolve(self):
-        if len(self.future) > 0 and len(self.queue) > 0:
-            future: asyncio.Future = self.future.pop(0)
-            result = self.queue.pop(0)
+        if len(self.futures_queue) > 0 and len(self.messages_queue) > 0:
+            future: asyncio.Future = self.futures_queue.pop(0)
+            result = self.messages_queue.pop(0)
             future.set_result(result)
 
 
